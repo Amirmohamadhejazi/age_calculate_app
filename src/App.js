3ec1dc5c46fcea29ee6136d35d08fc6ad43044ae
Amirmohamadhejazi <Amirmohamadhejazi.9731@gmail.com>
2023-04-05 01:27:27 +0330

    initial cmt

--- src/App.js
+++ src/App.js
@@ -1,24 +1,132 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import './assest/style.scss';
+import { TbArrowBigDownLines ,TbArrowBigDownLinesFilled} from 'react-icons/tb';
+import { useForm } from "react-hook-form"
+import {useEffect, useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [icon , SetIcon ] = useState(<TbArrowBigDownLines className="btnNotActive"/>)
+    const [dataForm , SetDataForm] = useState([
+        {
+            name :"year",
+            value:"",
+            id:0
+        },
+        {
+            name :"month",
+            value:"",
+            id:1
+        },
+        {
+            name :"day",
+            value:"",
+            id:2
+        },
+    ])
+    useEffect(()=>{
+        console.table("dataForm")
+        console.table(dataForm)
+        console.table("dataForm")
+    },[dataForm])
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+
+    const onSubmit = (data) => {
+        let today = new Date();
+        if (parseInt(data.year) ===  today.getFullYear()  ){
+            if (  parseInt(data.month) > today.getMonth()+1){
+                console.log("errore from month invalid")
+            }else if( parseInt(data.day) > today.getDate()){
+                console.log("errore from day invalid")
+            }else {
+                calculateAge(`${data.year}/${data.month}/${data.day}`)
+            }
+        }else {
+            calculateAge(`${data.year}/${data.month}/${data.day}`)
+        }
+
+    }
+    function calculateAge(date) {
+        let today = new Date();
+        let oldDate = new Date(date);
+        let years = today.getFullYear() - oldDate.getFullYear();
+        let months = today.getMonth() - oldDate.getMonth();
+        let days = today.getDate() - oldDate.getDate();
+
+        if (months < 0 || (months === 0 && days < 0)) {
+            years--;
+            months += 12;
+        }
+        if (days < 0) {
+            let monthDays = new Date(today.getFullYear(), today.getMonth(), 0).getDate();
+            days += monthDays;
+            months--;
+        }
+
+        SetIcon(<TbArrowBigDownLinesFilled  className="btnActive"/>)
+
+        setTimeout(()=>{
+            SetIcon(<TbArrowBigDownLines className="btnNotActive"/>)
+            SetDataForm([
+                {
+                    name :"year",
+                    value:years,
+                    id:0
+                },
+                {
+                    name :"month",
+                    value:months,
+                    id:1
+                },
+                {
+                    name :"day",
+                    value:days,
+                    id:2
+                },
+            ])
+        },1500)
+
+    }
+
+    return (
+      <div className="container">
+          <div className="w-full h-screen flex-center">
+
+              <div className="w-full h-[65%]" >
+                  <div className="box_container bg-white p-8 lg:p-24    ">
+                      <form onSubmit={handleSubmit(onSubmit)} className="">
+                          <div className="w-full flex justify-between gap-5 flex-wrap " >
+                              <div className="w-[100%] lg:w-[25%] ">
+                                  <label className={`block ${errors.day ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="day">day </label>
+                                  <input  className="custom-Input appearance-none " type="number" placeholder="DD" id="day" {...register("day", { required: true , max:31, min:1 })} />
+                                  <label className="block text-red-700 font-bold mt-2" htmlFor="day">{errors.day && " Must be a valid day"} </label>
+                              </div>
+                              <div className="w-[100%] lg:w-[25%]   ">
+                                  <label className={`block ${errors.month ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="month">month </label>
+                                  <input className="custom-Input  appearance-none" type="number" placeholder="MM" id="month" {...register("month", { required: true , max:12 , min:1})} />
+                                  <label className="block text-red-700 font-bold mt-2" htmlFor="month">{errors.month && " Must be a valid month"} </label>
+                              </div>
+                              <div className="w-[100%] lg:w-[25%] ">
+                                  <label className={`block ${errors.year ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="year">year </label>
+                                  <input  className="custom-Input appearance-none " type="number" placeholder="YYYY" id="year" {...register("year", { required: true , max:2023 , min:1000 , })} />
+                                  <label className="block text-red-700 font-bold mt-2" htmlFor="year">{errors.year && " Must be a valid day"} </label>
+                              </div>
+                          </div>
+
+                          <div className="h-[50px] my-[5rem] relative">
+                              <hr/>
+                              <button type="submit" className="btn_calc w-[100px] h-[100px]">{icon}</button>
+                          </div>
+                      </form>
+                      {errors.exampleRequired && <span>This field is required</span>}
+                      <div className="flex flex-col result_text">
+                          {
+                              dataForm.map((item)=><span className="c-cerulean-frost" key={item.id}><span className="c-lavender-indigo"> {item.value? item.value : "0"} </span>{item.name}</span>)
+                          }
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   );
 }
 
