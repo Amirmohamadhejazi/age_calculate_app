36133e32b2f35d54d1f4419493fec9b9f5e82180
Amirmohamadhejazi <Amirmohamadhejazi.9731@gmail.com>
2023-04-06 02:27:23 +0330

    fix design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import {useEffect, useState} from "react";
+import { useForm } from "react-hook-form"
+import Arrow_img from "./assest/images/icon-arrow.svg";
 import './index.css';
 import './assest/style.scss';
-import { TbArrowBigDownLines ,TbArrowBigDownLinesFilled} from 'react-icons/tb';
-import { useForm } from "react-hook-form"
-import {useEffect, useState} from "react";
+
+
 
 function App() {
-    const [icon , SetIcon ] = useState(<TbArrowBigDownLines className="btnNotActive"/>)
+    const [icon , SetIcon ] = useState(<img src={Arrow_img} className="btnNotActive" alt=""/>)
+    const [customError , SetCustomError ] = useState({m:false , d:false})
     const [dataForm , SetDataForm] = useState([
         {
             name :"year",
@@ -34,13 +37,17 @@ function App() {
         let today = new Date();
         if (parseInt(data.year) ===  today.getFullYear()  ){
             if (  parseInt(data.month) > today.getMonth()+1){
+                SetCustomError({...customError , m:true})
                 console.log("errore from month invalid")
             }else if( parseInt(data.day) > today.getDate()){
                 console.log("errore from day invalid")
+                SetCustomError({...customError , d:true})
             }else {
+                SetCustomError({m:false , d:false})
                 calculateAge(`${data.year}/${data.month}/${data.day}`)
             }
         }else {
+            SetCustomError({m:false , d:false})
             calculateAge(`${data.year}/${data.month}/${data.day}`)
         }
 
@@ -62,10 +69,9 @@ function App() {
             months--;
         }
 
-        SetIcon(<TbArrowBigDownLinesFilled  className="btnActive"/>)
-
+        SetIcon(<img src={Arrow_img} className="btnActive" alt=""/> )
         setTimeout(()=>{
-            SetIcon(<TbArrowBigDownLines className="btnNotActive"/>)
+            SetIcon(<img src={Arrow_img} className="btnNotActive" alt=""/>)
             SetDataForm([
                 {
                     name :"year",
@@ -89,24 +95,22 @@ function App() {
 
     return (
       <div className="container">
-          <div className="w-full h-screen flex-center">
-
-              <div className="w-full h-[65%]" >
-                  <div className="box_container bg-white p-8 lg:p-24    ">
+          <div className="w-full h-screen   flex-center">
+                  <div className="box_container bg-white p-5 lg:p-24    ">
                       <form onSubmit={handleSubmit(onSubmit)} className="">
-                          <div className="w-full flex justify-between gap-5 flex-wrap " >
-                              <div className="w-[100%] lg:w-[25%] ">
-                                  <label className={`block ${errors.day ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="day">day </label>
+                          <div className="w-full flex justify-between lg:justify-start gap-5   " >
+                              <div className="w-[32%] lg:w-[25%] ">
+                                  <label className={`block ${errors.day ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="day">DAY </label>
                                   <input  className="custom-Input appearance-none " type="number" placeholder="DD" id="day" {...register("day", { required: true , max:31, min:1 })} />
-                                  <label className="block text-red-700 font-bold mt-2" htmlFor="day">{errors.day && " Must be a valid day"} </label>
+                                  <label className="block text-red-700 font-bold mt-2" htmlFor="day">{(errors.day || customError.d === true) && " Must be a valid day"} </label>
                               </div>
-                              <div className="w-[100%] lg:w-[25%]   ">
-                                  <label className={`block ${errors.month ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="month">month </label>
+                              <div className="w-[32%] lg:w-[25%] ">
+                                  <label className={`block ${errors.month ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="month">MONTH </label>
                                   <input className="custom-Input  appearance-none" type="number" placeholder="MM" id="month" {...register("month", { required: true , max:12 , min:1})} />
-                                  <label className="block text-red-700 font-bold mt-2" htmlFor="month">{errors.month && " Must be a valid month"} </label>
+                                  <label className="block text-red-700 font-bold mt-2" htmlFor="month">{(errors.month || customError.m === true)  && " Must be a valid month"} </label>
                               </div>
-                              <div className="w-[100%] lg:w-[25%] ">
-                                  <label className={`block ${errors.year ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="year">year </label>
+                              <div className="w-[32%] lg:w-[25%] ">
+                                  <label className={`block ${errors.year ? 'text-red-700' : 'text-gray-700'} font-bold mb-2`}  htmlFor="year">YEAR </label>
                                   <input  className="custom-Input appearance-none " type="number" placeholder="YYYY" id="year" {...register("year", { required: true , max:2023 , min:1000 , })} />
                                   <label className="block text-red-700 font-bold mt-2" htmlFor="year">{errors.year && " Must be a valid day"} </label>
                               </div>
@@ -114,7 +118,7 @@ function App() {
 
                           <div className="h-[50px] my-[5rem] relative">
                               <hr/>
-                              <button type="submit" className="btn_calc w-[100px] h-[100px]">{icon}</button>
+                              <button type="submit" className="btn_calc   w-[80px] h-[80px]  lg:w-[100px] lg:h-[100px] ">{icon}</button>
                           </div>
                       </form>
                       {errors.exampleRequired && <span>This field is required</span>}
@@ -124,7 +128,6 @@ function App() {
                           }
                       </div>
                   </div>
-              </div>
           </div>
       </div>
   );
